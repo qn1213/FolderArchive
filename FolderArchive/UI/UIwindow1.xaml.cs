9cad3fd56e77b2a31d367d9e17440ebe6c50bc6b
Sejin Park <40251145+DevSejin@users.noreply.github.com>
2022-08-09 17:38:50 +0900

    UI from FASTHDL

---
+++ FolderArchive/UI/UIwindow1.xaml.cs
@@ -0,0 +1,64 @@
+﻿using FastHDLsearch.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace FolderArchive.UI
+{
+    /// <summary>
+    /// UIwindows.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class UIwindow1 : Window
+    {
+        Button[] tabbuttons;
+        public UIwindow1()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+            tabbuttons = new Button[] { xn_tabbutton1, xn_tabbutton2, xn_tabbutton3 };
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            if (outputPathSettings_combobox.SelectedIndex == 2)
+            {
+                outputPathSettings.IsEnabled = true;
+                outputPathSettings_button.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                outputPathSettings.IsEnabled = false;
+                outputPathSettings_button.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void xn_TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (var btit in tabbuttons.Select((value, index) => new { value, index }))
+            {
+                if (btit.index == xn_TabControl.SelectedIndex)
+                {
+                    btit.value.IsEnabled = false;
+                }
+                else
+                {
+                    btit.value.IsEnabled = true;
+                }
+
+            }
+        }
+    }
+}
---
+++ FolderArchive/UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FastHDLsearch.ViewModels
+{
+    internal class MainViewModel :INotifyPropertyChanged
+    {
+        public ObservableCollection<string> MyMessages { get; set; }
+
+        //public RelayCommand MessageBoxCommand { get; private set; }
+        public RelayCommand TabButtonCommand { get; set; }
+        public RelayCommand HyperlinkButtonCommand { get; set; }
+        private int _selectedIndex = 0;
+        public int SelectedIndex
+        {
+            get
+            {
+                return _selectedIndex;
+            }
+            set
+            {
+                _selectedIndex = value;
+                Notify("SelectedIndex");
+            }
+        }
+        private int _selectedbackgroundIndex=1;
+        public int SelectedbackgroundIndex
+        {
+            get
+            {
+                return _selectedbackgroundIndex;
+            }
+            set
+            {
+                _selectedbackgroundIndex = value;
+                Notify("SelectedbackgroundIndex");
+            }
+        }
+
+        public MainViewModel()
+        {
+            MyMessages = new ObservableCollection<string>()
+            {
+                "Hello World!",
+                "My name is Joe",
+                "I love learning commands",
+                "Im a message box",
+                "Im a console"
+            };
+
+            //MessageBoxCommand = new RelayCommand(DisplayInMessageBox, MessageBoxCanUse);
+            TabButtonCommand = new RelayCommand(TabButtonClicked);
+            SelectedbackgroundIndex = SelectedIndex + 1;
+            HyperlinkButtonCommand = new RelayCommand(OpenHyperlink);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Notify(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        //public void DisplayInMessageBox(object message)
+        //{
+        //    MessageBox.Show((string)message);
+        //}
+
+        public void TabButtonClicked(object index)
+        {
+            Debug.WriteLine("test"+SelectedIndex);
+            SelectedIndex = int.Parse(index.ToString() ?? "0");
+            SelectedbackgroundIndex = SelectedIndex+1;
+            
+        }
+        public void OpenHyperlink(object uri)
+        {
+            if (uri == "")
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo("https://null") { UseShellExecute = true });
+            }
+            else
+            {
+                System.Diagnostics.Process.Start(new ProcessStartInfo(uri.ToString() ?? "https://null") { UseShellExecute = true });
+            }
+            
+        }
+
+        public bool MessageBoxCanUse(object message)
+        {
+            if ((string)message == "Im a console")
+                return false;
+
+            return true;
+        }
+
+        public bool ConsoleCanUse(object message)
+        {
+            if ((string)message == "Im a message box")
+                return false;
+
+            return true;
+        }
+    }
+}
